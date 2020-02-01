d819c218711196511cbdaaced24c3545b7cffa2b
Xin Wang <xin.victorwang@gmail.com>
2020-02-02 05:07:29 +0800

    add button

--- site_config/home.js
+++ site_config/home.js
@@ -60,7 +60,7 @@ export default {
                     link: 'https://github.com/wuhan2020/wuhan2020',
                     type: 'normal',
                 },
-                {
+                    {
                     text: 'Platform for Wuhan 2019-nCoV Epidemic Prevention',
                     link: 'https://wuhan2020.kaiyuanshe.cn/',
                     type: 'normal',
