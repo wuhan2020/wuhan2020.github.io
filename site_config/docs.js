25ea2b4e3ab1a486b34b12b7784eb41096c124a2
Xin Wang <xin.victorwang@gmail.com>
2020-02-02 05:43:29 +0800

    Tidy css (#13)

--- site_config/docs.js
+++ site_config/docs.js
@@ -29,7 +29,23 @@ export default {
                 children: [
                     {
                         title: 'Quick Start',
-                        link: '/en-us/docs/user/quickstart.html',
+                        link: '/en-us/docs/dev/quickstart.html',
+                    },
+                    {
+                        title: 'Data Collection Guide',
+                        link: '/en-us/docs/dev/information-guide.html',
+                    },
+                    {
+                        title: 'Architecture Diagram',
+                        link: '/en-us/docs/dev/architecture.html',
+                    },
+                    {
+                        title: 'Guide For contribution',
+                        link: '/en-us/docs/dev/contributing.html',
+                    },
+                    {
+                        title: 'Github Repo processing',
+                        link: '/en-us/docs/dev/dev-env-setup.html',
                     },
                     {
                         title: 'Data Collection Guide',
@@ -82,7 +98,7 @@ export default {
                 children: [
                     {
                         title: '快速开始',
-                        link: '/zh-cn/docs/user/quickstart.html',
+                        link: '/zh-cn/docs/dev/quickstart.html',
                     },
                     {
                         title: '信息收集指南',
