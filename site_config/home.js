4ed71807d9b3db6a99374863d191607878e14d0e
Rui Chen <chenrui333@gmail.com>
2020-02-02 01:30:35 -0500

    Update jp and it config in home.js

--- site_config/home.js
+++ site_config/home.js
@@ -1,27 +1,4 @@
 export default {
-    'zh-cn': {
-        brand: {
-            brandName: 'Wuhan2020',
-            briefIntroduction: '我们当前在做的事是：建立一个医院、工厂、采购等信息实时同步的数据服务，同时召集所有希望对这次抗击病毒战役进行贡献的人，让每个具有相关技能的人都可以参与相关主题的开发工作，用开源的社区文化，以自组织协作的方式完成。',
-            buttons: [
-                {
-                    text: '快速开始',
-                    link: '/zh-cn/docs/overview/what-is-wuhan2020.html',
-                    type: 'primary',
-                },
-                {
-                    text: 'Github',
-                    link: 'https://github.com/wuhan2020/wuhan2020',
-                    type: 'normal',
-                },
-                {
-                    text: '武汉新型冠状病毒防疫信息收集平台',
-                    link: 'https://wuhan2020.kaiyuanshe.cn/',
-                    type: 'normal',
-                },
-            ],
-        },
-    },
     'en-us': {
         brand: {
             brandName: 'Wuhan2020',
@@ -48,10 +25,10 @@ export default {
     'it-it': {
         brand: {
             brandName: 'Wuhan2020',
-            briefIntroduction: 'What we are doing now is: to establish a real-time and synchronous data service for hospitals, factories, procurement and other information, and to convene all those who want to contribute to the anti-virus campaign, so that everyone with relevant skills can participate in the development of related topics, and complete it in a self-organized and collaborative way with open source community culture.',
+            briefIntroduction: 'Quello che stiamo facendo ora è: creare un servizio di dati sincrono e in tempo reale per ospedali, fabbriche, approvvigionamento e altre informazioni e convocare tutti coloro che vogliono contribuire alla campagna antivirus, in modo che chiunque abbia le competenze pertinenti possa partecipare allo sviluppo di argomenti correlati e completarlo in modo auto-organizzato e collaborativo con la cultura della comunità open source.',
             buttons: [
                 {
-                    text: 'Get Started',
+                    text: 'Iniziare',
                     link: '/it-it/docs/overview/what-is-wuhan2020.html',
                     type: 'primary',
                 },
@@ -61,7 +38,7 @@ export default {
                     type: 'normal',
                 },
                     {
-                    text: 'Platform for Wuhan 2019-nCoV Epidemic Prevention',
+                    text: "Piattaforma per la prevenzione dell'epidemia di Wuhan 2019-nCoV",
                     link: 'https://wuhan2020.kaiyuanshe.cn/',
                     type: 'normal',
                 },
@@ -71,10 +48,10 @@ export default {
     'ja-jp': {
         brand: {
             brandName: 'Wuhan2020',
-            briefIntroduction: 'What we are doing now is: to establish a real-time and synchronous data service for hospitals, factories, procurement and other information, and to convene all those who want to contribute to the anti-virus campaign, so that everyone with relevant skills can participate in the development of related topics, and complete it in a self-organized and collaborative way with open source community culture.',
+            briefIntroduction: '現在私たちがしていることは、病院、工場、調達、その他の情報のためのリアルタイムの同期データサービスを確立し、関連するスキルを持つすべての人が 関連トピックの開発に参加し、オープンソースのコミュニティ文化と自己組織化された共同の方法でそれを完了します。',
             buttons: [
                 {
-                    text: 'Get Started',
+                    text: '始めましょう',
                     link: '/ja-jp/docs/overview/what-is-wuhan2020.html',
                     type: 'primary',
                 },
@@ -84,11 +61,34 @@ export default {
                     type: 'normal',
                 },
                 {
-                    text: 'Platform for Wuhan 2019-nCoV Epidemic Prevention',
+                    text: '武漢2019-nCoV流行防止のプラットフォーム',
                     link: 'https://wuhan2020.kaiyuanshe.cn/',
                     type: 'normal',
                 },
             ],
         }
+    },
+    'zh-cn': {
+        brand: {
+            brandName: 'Wuhan2020',
+            briefIntroduction: '我们当前在做的事是：建立一个医院、工厂、采购等信息实时同步的数据服务，同时召集所有希望对这次抗击病毒战役进行贡献的人，让每个具有相关技能的人都可以参与相关主题的开发工作，用开源的社区文化，以自组织协作的方式完成。',
+            buttons: [
+                {
+                    text: '快速开始',
+                    link: '/zh-cn/docs/overview/what-is-wuhan2020.html',
+                    type: 'primary',
+                },
+                {
+                    text: 'Github',
+                    link: 'https://github.com/wuhan2020/wuhan2020',
+                    type: 'normal',
+                },
+                {
+                    text: '武汉新型冠状病毒防疫信息收集平台',
+                    link: 'https://wuhan2020.kaiyuanshe.cn/',
+                    type: 'normal',
+                },
+            ],
+        },
     },
 };
