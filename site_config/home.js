b8e2673d5f4080d7a68ef2fe4a6370868b60e43f
lovepoem <lovepoem@users.noreply.github.com>
2020-02-04 01:10:50 +0000

    Automated deployment: Tue Feb  4 01:10:50 UTC 2020 029f4bc0a43b263c7dfa0afdbc071ed72df9b241

--- site_config/home.js
+++ site_config/home.js
@@ -1,154 +1,94 @@
 export default {
-  'en-us': {
-    brand: {
-      brandName: 'What is the community of wuhan2020 ?',
-      briefIntroduction: 'What we are doing now is: to establish a real-time and synchronous data service for hospitals, factories, procurement and other information, and to convene all those who want to contribute to the anti-virus campaign, so that everyone with relevant skills can participate in the development of related topics, and complete it in a self-organized and collaborative way with open source community culture.',
-      buttons: [
-        {
-          text: 'Get Started',
-          link: '/en-us/docs/overview/what-is-wuhan2020.html',
-          type: 'primary',
-        },
-        {
-          text: 'Github',
-          link: 'https://github.com/wuhan2020/wuhan2020',
-          type: 'normal',
-        },
-        {
-          text: 'Platform for Wuhan 2019-nCoV Epidemic Prevention',
-          link: 'https://wuhan2020.kaiyuanshe.cn/',
-          type: 'normal',
-        },
-      ],
-    },
-    features: {
-      title: 'Community introduction',
-      list: [
-        {
-          icon: 'feature-1',
-          title: '2019-ncov information platform',
-          content: 'An information system developed by the community. Facilitate the information exchange between resource providers (medical resource donors, logistics companies, hotels, free clinic resources) and those who need help (hospitals, people in the disaster area), so as to facilitate and quickly help the people in the disaster area',
-        },
-        {
-          icon: 'feature-2',
-          title: 'Wuhan2020 community',
-          content: 'At present, there are more than 3000 volunteers from different countries in Wuhan 2020 community. There are programmers, product managers, designers, project managers, data analysts, information collectors, translators, media workers, students, etc. Through the establishment of information system, data analysis and prediction, it makes contribution to the resistance of new coronavirus.',
+    'en-us': {
+        brand: {
+            brandName: 'Wuhan2020',
+            briefIntroduction: 'What we are doing now is: to establish a real-time and synchronous data service for hospitals, factories, procurement and other information, and to convene all those who want to contribute to the anti-virus campaign, so that everyone with relevant skills can participate in the development of related topics, and complete it in a self-organized and collaborative way with open source community culture.',
+            buttons: [
+                {
+                    text: 'Get Started',
+                    link: '/en-us/docs/overview/what-is-wuhan2020.html',
+                    type: 'primary',
+                },
+                {
+                    text: 'Github',
+                    link: 'https://github.com/wuhan2020/wuhan2020',
+                    type: 'normal',
+                },
+                {
+                    text: 'Platform for Wuhan 2019-nCoV Epidemic Prevention',
+                    link: 'https://wuhan2020.kaiyuanshe.cn/',
+                    type: 'normal',
+                },
+            ],
         }
-      ],
     },
-  },
-  'it-it': {
-    brand: {
-      brandName: 'Qual è la comunità di Wuhan2020 ? ',
-      briefIntroduction: 'Quello che stiamo facendo ora è: creare un servizio di dati sincrono e in tempo reale per ospedali, fabbriche, approvvigionamento e altre informazioni e convocare tutti coloro che vogliono contribuire alla campagna antivirus, in modo che chiunque abbia le competenze pertinenti possa partecipare allo sviluppo di argomenti correlati e completarlo in modo auto-organizzato e collaborativo con la cultura della comunità open source.',
-      buttons: [
-        {
-          text: 'Iniziare',
-          link: '/it-it/docs/overview/what-is-wuhan2020.html',
-          type: 'primary',
-        },
-        {
-          text: 'Github',
-          link: 'https://github.com/wuhan2020/wuhan2020',
-          type: 'normal',
-        },
-        {
-          text: "Piattaforma per la prevenzione dell'epidemia di Wuhan 2019-nCoV",
-          link: 'https://wuhan2020.kaiyuanshe.cn/',
-          type: 'normal',
-        },
-      ],
-    },
-    features: {
-      title: '社区介绍',
-      list: [
-        {
-          icon: 'feature-1',
-          title: 'Piattaforma informativa 2019-ncov',
-          content: 'Un sistema di informazione sviluppato dalla comunità.Facilitare lo scambio di informazioni tra i fornitori di risorse (donatori di risorse mediche, società logistiche, alberghi, risorse cliniche gratuite) e coloro che hanno bisogno di aiuto (ospedali, persone nella zona delle catastrofi), in modo da facilitare e rapidamente aiutare le persone nella zona delle catastrofi',
-        },
-        {
-          icon: 'feature-2',
-          title: 'Comunità Wuhan2020',
-          content: 'Attualmente vi sono più di 3000 volontari provenienti da diversi paesi della comunità Wuhan 2020.Ci sono programmatori, product manager, progettisti, project manager, analizzatori di dati, collezionisti di informazioni, traduttori, operatori di media, studenti, ecc.Attraverso l\'istituzione di un sistema informativo, l\'analisi dei dati e la previsione, contribuisce alla resistenza del nuovo coronavirus.',
+    'it-it': {
+        brand: {
+            brandName: 'Wuhan2020',
+            briefIntroduction: 'Quello che stiamo facendo ora è: creare un servizio di dati sincrono e in tempo reale per ospedali, fabbriche, approvvigionamento e altre informazioni e convocare tutti coloro che vogliono contribuire alla campagna antivirus, in modo che chiunque abbia le competenze pertinenti possa partecipare allo sviluppo di argomenti correlati e completarlo in modo auto-organizzato e collaborativo con la cultura della comunità open source.',
+            buttons: [
+                {
+                    text: 'Iniziare',
+                    link: '/it-it/docs/overview/what-is-wuhan2020.html',
+                    type: 'primary',
+                },
+                {
+                    text: 'Github',
+                    link: 'https://github.com/wuhan2020/wuhan2020',
+                    type: 'normal',
+                },
+                {
+                    text: "Piattaforma per la prevenzione dell'epidemia di Wuhan 2019-nCoV",
+                    link: 'https://wuhan2020.kaiyuanshe.cn/',
+                    type: 'normal',
+                },
+            ],
         }
-      ],
     },
-  },
-  'ja-jp': {
-    brand: {
-      brandName: 'Wuhan 2020コミュニティは何をしていますか？',
-      briefIntroduction: '現在私たちがしていることは、病院、工場、調達、その他の情報のためのリアルタイムの同期データサービスを確立し、関連するスキルを持つすべての人が 関連トピックの開発に参加し、オープンソースのコミュニティ文化と自己組織化された共同の方法でそれを完了します。',
-      buttons: [
-        {
-          text: '始めましょう',
-          link: '/ja-jp/docs/overview/what-is-wuhan2020.html',
-          type: 'primary',
-        },
-        {
-          text: 'Github',
-          link: 'https://github.com/wuhan2020/wuhan2020',
-          type: 'normal',
-        },
-        {
-          text: '武漢2019-nCoV流行防止のプラットフォーム',
-          link: 'https://wuhan2020.kaiyuanshe.cn/',
-          type: 'normal',
-        },
-      ],
-    },
-    features: {
-      title: 'コミュニティの紹介',
-      list: [
-        {
-          icon: 'feature-1',
-          title: '武漢の新型の冠状ウイルス防疫情報プラットフォーム',
-          content: 'コミュニティ開発の情報システムです。資源提供者（医療資源提供者、物流会社、ホテル、義診資源）と協力者（病院、被災地の大衆）との間で情報を交換し、被災者の救助を迅速に行う。',
-        },
-        {
-          icon: 'feature-2',
-          title: 'Wuhan 2020コミュニティ',
-          content: '現在Wuhan 2020コミュニティには様々な国から3000人以上のボランティアが集まっています。プログラマ、プロダクトマネージャー、デザイナー、プロジェクトマネージャー、データアナリスト、情報収集者、翻訳、メディア関係者、学生などがいます。情報システムの構築、データ解析予測などにより、新型冠状ウイルスへの抵抗に貢献します。',
+    'ja-jp': {
+        brand: {
+            brandName: 'Wuhan2020',
+            briefIntroduction: '現在私たちがしていることは、病院、工場、調達、その他の情報のためのリアルタイムの同期データサービスを確立し、関連するスキルを持つすべての人が 関連トピックの開発に参加し、オープンソースのコミュニティ文化と自己組織化された共同の方法でそれを完了します。',
+            buttons: [
+                {
+                    text: '始めましょう',
+                    link: '/ja-jp/docs/overview/what-is-wuhan2020.html',
+                    type: 'primary',
+                },
+                {
+                    text: 'Github',
+                    link: 'https://github.com/wuhan2020/wuhan2020',
+                    type: 'normal',
+                },
+                {
+                    text: '武漢2019-nCoV流行防止のプラットフォーム',
+                    link: 'https://wuhan2020.kaiyuanshe.cn/',
+                    type: 'normal',
+                },
+            ],
         }
-      ],
     },
-  },
-  'zh-cn': {
-    brand: {
-      brandName: 'Wuhan2020 社区是做什么的 ?',
-      briefIntroduction: '我们当前在做的事是：建立一个医院、工厂、采购等信息实时同步的数据服务，同时召集所有希望对这次抗击病毒战役进行贡献的人，让每个具有相关技能的人都可以参与相关主题的开发工作，用开源的社区文化，以自组织协作的方式完成。',
-      buttons: [
-        {
-          text: '快速开始',
-          link: '/zh-cn/docs/overview/what-is-wuhan2020.html',
-          type: 'primary',
+    'zh-cn': {
+        brand: {
+            brandName: 'Wuhan2020',
+            briefIntroduction: '我们当前在做的事是：建立一个医院、工厂、采购等信息实时同步的数据服务，同时召集所有希望对这次抗击病毒战役进行贡献的人，让每个具有相关技能的人都可以参与相关主题的开发工作，用开源的社区文化，以自组织协作的方式完成。',
+            buttons: [
+                {
+                    text: '快速开始',
+                    link: '/zh-cn/docs/overview/what-is-wuhan2020.html',
+                    type: 'primary',
+                },
+                {
+                    text: 'Github',
+                    link: 'https://github.com/wuhan2020/wuhan2020',
+                    type: 'normal',
+                },
+                {
+                    text: '武汉新型冠状病毒防疫信息收集平台',
+                    link: 'https://wuhan2020.kaiyuanshe.cn/',
+                    type: 'normal',
+                },
+            ],
         },
-        {
-          text: 'Github',
-          link: 'https://github.com/wuhan2020/wuhan2020',
-          type: 'normal',
-        },
-        {
-          text: '武汉新型冠状病毒防疫信息平台',
-          link: 'https://wuhan2020.kaiyuanshe.cn/',
-          type: 'normal',
-        },
-      ],
     },
-    features: {
-      title: '社区介绍',
-      list: [
-        {
-          icon: 'feature-1',
-          title: '新型冠状病毒防疫信息平台',
-          content: '社区研发的一个信息系统。方便资源提供者(医疗资源捐献者、物流公司、酒店、义诊资源)和需要帮助者（医院、灾区群众）之间信息互通，以方便快速地使灾区人民得到救助',
-        },
-        {
-          icon: 'feature-2',
-          title: 'Wuhan2020社区',
-          content: '目前Wuhan2020社区有来自不同国家的3000多志愿者组成。有程序员、产品经理、设计师、项目经理、数据分析师、信息采集者、翻译、媒体工作者、学生等。通过建立信息系统、数据分析预测等为抵抗新型冠状病毒做贡献。',
-        }
-      ],
-    },
-  },
 };
