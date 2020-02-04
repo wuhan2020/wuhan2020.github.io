f229d60e8489c827477269cb5f49843db72d510e
GindaChen <GindaChen@users.noreply.github.com>
2020-02-04 23:45:05 +0000

    Automated deployment: Tue Feb  4 23:45:05 UTC 2020 e5f6f5ded6f34ac9e581bdc9a85cb1d31e6ece21

--- site_config/home.js
+++ site_config/home.js
@@ -39,7 +39,7 @@ export default {
   },
   'it-it': {
     brand: {
-      brandName: 'Di cosa si occupa la community Wuhan2020?',
+      brandName: 'Cosa fa la community Wuhan2020?',
       briefIntroduction: 'Ciò che stiamo facendo è creare un servizio informazioni, aggiornato in tempo reale, relativo ad ospedali e supporto medico, attività produttive, servizi di approvvigionamento e molto altro. Allo stesso tempo, stiamo reclutando tutti coloro che desiderano dare un contributo alla campagna contro il coronavirus, in modo che tutte le persone che possiedono le adeguate competenze possano contribuire al lavoro di sviluppo nell\'ambito delle tematiche ad esse pertinenti e quindi, in accordo con la cultura della comunità Open Source, al raggiungimento del successo attraverso un approccio di cooperazione autogestita.',
       buttons: [
         {
