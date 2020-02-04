b4ad362441264235f072eed9dc43839e6bd6ec8e
Alan Luo <i@iltc.io>
2020-02-04 09:26:33 -0500

    Fixed Icon Problem

--- src/pages/home/featureItem.jsx
+++ src/pages/home/featureItem.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Icon from '../../components/icon';
 
 const Item = (props) => {
   const { feature } = props;
   return (
     <li>
+      <Icon type={feature.icon} />
       <div>
         <h4>{feature.title}</h4>
         <p>{feature.content}</p>
