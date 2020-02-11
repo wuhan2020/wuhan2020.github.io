7050575b90fa2913d72f0517ac76d98dc029d32a
gakki88 <tangtang5yo@gmail.com>
2020-02-11 14:13:13 +0000

    项目列表页翻译中文---法文 (#203)

--- site_config/project.js
+++ site_config/project.js
@@ -144,36 +144,39 @@ export default {
         },
     },
     'fr-fr': {
-        barText: 'Site web',
-        sectionDescription: '',
+        barText: 'Projets',
+        sectionDescription: 'La communauté a déjà contribué aux projets open-source suivants',
         projects: {
             list: [
                 {
                     img: '/images/blog/wuhan2020.png',
-                    title: '新型冠状病毒防疫信息收集平台',
+
+                    title: "Plateforme de collecte d'informations anti-épidémiques 2019-nCoV",
                     repoLink:'https://github.com/wuhan2020/wuhan2020',
-                    description: "Plateforme de collecte d'informations anti-épidémiques 2019-nCoV",
+                    description: "Une plateforme d'information dévélopée par la communauté pour faciliter la communication entre les fournisseurs (dons des ressources médicales, entreprises de transport et logistique, hôtels, cliniques gratuites) et les parties qui en ont besoin (hôpitaux, population dans les zones touchées), pour que les gens dans les zones touchées peuvent avoir des aides rapidement.",
                     link: 'https://wh.opensource-service.cn/',
                 },
                 {
                   img: '/images/blog/wuhan2020.png',
-                  title: '2020援助武汉',
+
+                  title: '2020 Aider Wuhan',
                   repoLink: 'https://github.com/wuhan2020/data-sync',
-                  description: '建立一个医院、工厂、采购等信息实时同步的数据服务，同时召集所有希望对这次抗击病毒战役进行贡献的人，让每个具有相关技能的人都可以参与相关主题的开发工作，用开源的社区文化，以自组织协作的方式完成。',
+                  description: 'Créer un service de données synchronisées en temps réel sur les informations des hôpitaux, des usines, des achats etc, en même temps de rassembler tous ceux qui souhaitent contribuer à cette lutte contre le virus. Afin que tous ceux qui ont les compétences nécessaire puissent participer au dévéloppement sur les sujets concernés, avec une culture communautaire open-source et en collaboration auto-organisée.',
                   link: 'https://wuhan2020.kaiyuanshe.cn/',
                 },
                 {
                   img: '/images/blog/wuhan2020.png',
-                  title: 'Wuhan2020开源社区官网',
+
+                  title: 'Wuhan2020 Site officiel de la communauté',
                   repoLink: 'https://github.com/wuhan2020/wuhan2020.github.io',
-                  description: '目前Wuhan2020社区官方网站，召集不同国家不同职业的志愿者，建立开源信息系统，为抵抗新型冠状病毒做贡献。',
+                  description: "Le site officiel de la communauté de Wuhan2020 convoque les volontaires de différentes professions et de différentes nationalités pour créer le système d'information et contribuer à la lutte contre le nouveau coronavirus.",
                   link: 'https://community.wuhan2020.org.cn/',
                 },
                 {
                   img: '/images/blog/wuhan2020.png',
-                  title: '2019-nCoV-Nav 疫情信息导航',
+                  title: "2019-nCoV-Nav Navigation sur les informations d'épidémie",
                   repoLink: 'https://github.com/wuhan2020/2019-nCoV-Nav',
-                  description: '疫情信息导航网站为公众提供一个便利而全面的疫情信息入口，整理了涵盖疫情信息的各个方面的网络通道。',
+                  description: "Le site web de navigation des informations d'épidémie offre au public un accès facile et complet aux informations sur l'épidémie, en collationnant les informations de différents aspects sur l'épidémie.",
                   link: 'http://nav.werty.cn',
                 },
             ],
