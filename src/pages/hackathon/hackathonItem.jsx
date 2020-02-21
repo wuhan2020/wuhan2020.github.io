b7ed054e632376afd35360a3df46cfc5018d6dcd
Haoyue <wsunshine1089@163.com>
2020-02-21 19:33:13 +0800

    update hackathon

--- src/pages/hackathon/hackathonItem.jsx
+++ src/pages/hackathon/hackathonItem.jsx
@@ -3,18 +3,18 @@ import React from 'react';
 
 class HackathonItem extends React.Component {
   render() {
-    const { hackathon } = this.props;
+    // const { hackathon } = this.props;
     return (
       <div className="hackathon-item">
         {/* <div className="img-container">
           <img src={getLink(hackathon.img)} />
         </div> */}
-        <div className="hackathon-text-container">
+        {/* <div className="hackathon-text-container"> */}
           {/* <h4><a href={hackathon.link}>{hackathon.title}</a></h4> */}
           {/* <p>Github: <a href={hackathon.repoLink}>{hackathon.repoLink}</a></p> */}
-          <p>{hackathon.description}</p>
+          {/* <p>{hackathon.description}</p> */}
         </div>
-      </div>
+      // </div>
     );
   }
 }
