5a3018a53d674406c83bce7f8c9cb39c02e86ece
zhaofeng-shu33 <616545598@qq.com>
2020-02-28 12:47:49 +0800

    change one co-organizer

--- src/pages/hackathon/index.jsx
+++ src/pages/hackathon/index.jsx
@@ -225,10 +225,10 @@ class Hackathon extends Language {
 			
 			<div id="holder">
 				<h1>协办方</h1>
-				<div className="h1"><img src="/images/hackathon/holder_coorperation/holder_coorperation1.jpeg" /></div>
-				<div className="h2"><img src="/images/hackathon/holder_coorperation/holder_coorperation2.jpeg" /></div>
-				<div className="h3"><img src="/images/hackathon/holder_coorperation/holder_coorperation3.jpeg" /></div>
-				<div className="h4"><img src="/images/hackathon/holder/holder4.png" /></div>
+				<div><a href="https://www.freecodecamp.org/"><img src="/images/holder_coorperation/holder_coorperation1.jpeg" /></a></div>
+				<div><a href="http://ceoceo.org/"><img src="/images/holder_coorperation/holder_coorperation2.jpeg" /></a></div>
+				<div><a href="https://www.linkstars.com/"><img src="/images/holder_coorperation/holder_coorperation3.jpeg" /></a></div>
+				<div><a href="https://www.agora.io/"><img src="/images/holder_coorperation/holder_coorperation4.jpeg" /></a></div>
 			</div>
 			
 			<div id="holder">
