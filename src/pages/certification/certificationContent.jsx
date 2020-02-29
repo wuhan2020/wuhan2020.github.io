267416afe4a603d015b509a553be894483a5cc39
zhaofeng-shu33 <616545598@qq.com>
2020-02-29 11:57:02 +0800

    diff then use pr change

--- src/pages/certification/certificationContent.jsx
+++ src/pages/certification/certificationContent.jsx
@@ -6,31 +6,20 @@ class CertificationContent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-<<<<<<< HEAD
             status: 'notfoundWarn',
             isShowImage: false,
         };
-=======
-            status: 'notfoundWarn'
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
->>>>>>> dev
     }
     getAlertType(status) {
         let type = ['error', 'warn'].filter( type => status.toLowerCase().includes(type))[0];
         return  type || 'successful';
     }
 
-<<<<<<< HEAD
     handleSubmit = (event) => {
-=======
-    handleSubmit(event) {
->>>>>>> dev
         event.preventDefault();
         this.setState({
             status: 'userError'
         });
-<<<<<<< HEAD
     };
     
     handleResizing = () => {
@@ -54,17 +43,10 @@ class CertificationContent extends React.Component {
     render() {
         const { formText, alertMsgs } = this.props.dataSource;
         const alertType = this.getAlertType(this.state.status);
-=======
-    }
-    render() {
-        const { formText, alertMsgs } = this.props.dataSource;
-        
->>>>>>> dev
         return (
             <section>
                 <div className="certification-content">
                     <div className="certification-content-alerts row">
-<<<<<<< HEAD
                         <span className={`alert ${alertType}`}>{alertMsgs[this.state.status]}</span>
                     </div>
                     <div className="certification-content-main row">
@@ -76,16 +58,6 @@ class CertificationContent extends React.Component {
                                 </figure>
                             </aside>
                         }
-=======
-                        <span className={`alert ${this.getAlertType(this.state.status)}`}>{alertMsgs[this.state.status]}</span>
-                    </div>
-                    <div className="certification-content-main row">
-                        <aside className="column">
-                            <figure>
-                                <img src="/images/certification/illustration.png" />
-                            </figure>
-                        </aside>
->>>>>>> dev
                         <form className="column" onSubmit={this.handleSubmit}>
                             <fieldset>
                                 <legend>{formText.header}</legend>
@@ -94,14 +66,9 @@ class CertificationContent extends React.Component {
                                     <input type="email" name="mail" />
                                 </div>
                                 <div className="form-row name">
-<<<<<<< HEAD
                                     <label>{formText.nicknameLabel}
                                         <span className="description">{formText.nicknameDescription}</span>
                                     </label>
-=======
-                                    <label>{formText.nicknameLabel}</label>
-                                    <span className="description">{formText.nicknameDescription}</span>
->>>>>>> dev
                                     <input type="text" name="name" />
                                 </div>
                                 <div className="form-row action">
