af5396732adee2ae06ca6b642a78d0585e0b6f29
pengcc <pengcc981@gmail.com>
2020-03-07 01:06:16 +0100

    kanban: fix timer and and update style, fix #376

--- src/pages/hackathonKanban/kanbanTimer.jsx
+++ src/pages/hackathonKanban/kanbanTimer.jsx
@@ -1,54 +1,65 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import './kanbanTimer.scss';
 
-const propTypes = {
-    title: PropTypes.string.isRequired,
-    from: PropTypes.string.isRequired, // time string
-    timeSlots: PropTypes.array.isRequired, // [{ unit: 'd', text: '天'}， { unit: 'h', text: '时'}]
-};
+class KanbanTimer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.getTimeDiffFrom = this.getTimeDiffFrom.bind(this);
+        this.state = {
+            diff: this.getTimeDiffFrom(this.props.from),
+            interval: null,
+        };
+    }
 
-const getTimeDiffFrom = (start) => {
-    let diffMs = new Date().getTime() - new Date(start).getTime();
-    let diminution = 0;
-    const [dayMs, hourMs, minuteMs, secondMs] = [1000 * 60 * 60 * 24, 1000 * 60 * 60, 1000 * 60, 1000 ];
-    let d = Math.floor(diffMs / dayMs);
-    diminution += d * dayMs;
-    let h = Math.floor((diffMs - diminution) / hourMs);
-    diminution += h * hourMs;
-    let m = Math.floor((diffMs - diminution) / minuteMs);
-    diminution += m * minuteMs;
-    let s = Math.floor((diffMs - diminution) / secondMs);
+    getTimeDiffFrom = (start) => {
+        let diffMs = new Date().getTime() - new Date(start).getTime();
+        let diminution = 0;
+        const [dayMs, hourMs, minuteMs, secondMs] = [1000 * 60 * 60 * 24, 1000 * 60 * 60, 1000 * 60, 1000 ];
+        let d = Math.floor(diffMs / dayMs);
+        diminution += d * dayMs;
+        let h = Math.floor((diffMs - diminution) / hourMs);
+        diminution += h * hourMs;
+        let m = Math.floor((diffMs - diminution) / minuteMs);
+        diminution += m * minuteMs;
+        let s = Math.floor((diffMs - diminution) / secondMs);
+    
+        return {d, h, m, s};
+    };
+    
+    componentDidMount() {
+        const { from } = this.props;
+        const interval = setInterval(() => {
+                const diff = this.getTimeDiffFrom(from);
+                this.setState({ diff });
+        }, 1000); 
 
-    return {d, h, m, s};
-};
+        this.setState( {interval} );   
+    }
 
-const KanbanTimer = (props) => {
-    const { title, from, timeSlots } = props;
-    const timeDiff = getTimeDiffFrom(from);
-    return (
-        <div className="timer-wrapper">
-            <div className="timer-title">
-                {title}
+    render() {
+        const { title, timeSlots } = this.props;
+        const { diff } = this.state;
+        return (
+            <div className="timer-wrapper">
+                <div className="timer-title">
+                    {title}
+                </div>
+                <div className="timer-board">
+                    {
+                        timeSlots.map( ({unit, text} = {}, idx) => {
+                            return (
+                                <div className="time-slot" key={idx}>
+                                    <span className="time">{diff[unit]}</span>
+                                    <span className="text">{text}</span>
+                                </div>
+                            );
+                        })
+                    }
+                </div>
             </div>
-            <div className="timer-board">
-                {
-                    timeSlots.map( ({unit, text} = {}, idx) => {
-                        return (
-                            <div className="time-slot" key={idx}>
-                                <span className="time">{timeDiff[unit]}</span>
-                                <span className="text">{text}</span>
-                            </div>
-                        );
-                    })
-                }
-            </div>
-        </div>
-       
-    );
-};
+        );
+    }
+}
 
-KanbanTimer.propTypes = propTypes;
-
-export default KanbanTimer
+export default KanbanTimer;
