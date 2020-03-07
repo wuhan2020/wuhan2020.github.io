9abd6c9098ca7a53b3c7388aa9a3719a86f1841e
wonder <wonderland.dl@gmail.com>
2020-03-08 00:07:50 +0800

    finish hackthon progress (#402)
    * Update coronavirus-protection-guide.md
    
    * Update how-to-join-wuhan2020.md
    
    * Update nav-project-introduced.md
    
    * Update need-questionnaire.md
    
    * Update to-all-members.md
    
    * Update wuhan2020-media-channel.md
    
    * Update wuhan2020-official-announcement.md
    
    * 修改blog时间
    
    * fixed node@12 & gule@3.9.1
    
    * hackthonProgress done
    
    * 修改时间格式
    
    Co-authored-by: huangdeliang <huangdeliang@skieer.com>
    Co-authored-by: Xin Wang <xin.victorwang@gmail.com>

--- src/pages/hackathonKanban/index.jsx
+++ src/pages/hackathonKanban/index.jsx
@@ -5,7 +5,30 @@ import KanbanTimer from './kanbanTimer';
 import Language from "../../components/language";
 import Header from '../../components/header';
 import Footer from '../../components/footer';
+import HackathonProgress from './hackathonProgress';
 import './index.scss';
+
+const mockMilestones = [{
+    time: '02/28',
+    event: '热身',
+    color: ''
+}, {
+    time: '02/29',
+    event: '注册 入群 预热 组队',
+    color: ''
+}, {
+    time: '03/06',
+    event: '初赛',
+    color: ''
+}, {
+    time: '03/07',
+    event: '提交项目截止',
+    color: ''
+}, {
+    time: '03/08',
+    event: '决赛',
+    color: ''
+}]
 
 class HackathonKanban extends Language {
 
@@ -13,11 +36,11 @@ class HackathonKanban extends Language {
         const language = this.language;
         const dataSource = this.getLanguageDict(['zh-cn'], 'hackathonKanban');
         const { kanbanTimer, contentTitle, badgeCards } = dataSource;
-      
+
         // Data for 11 badge cards (divided into 3 sections)
-        const firstItemList = ['country', 'participant', 'team', 'star'].map( item => badgeCards[item] );
-        const secondItemList = ['visits', 'audience', 'community', 'exposure'].map( item => badgeCards[item] );
-        const thirdItemList = ['media', 'volunteers', 'vip'].map( item => badgeCards[item] );
+        const firstItemList = ['country', 'participant', 'team', 'star'].map(item => badgeCards[item]);
+        const secondItemList = ['visits', 'audience', 'community', 'exposure'].map(item => badgeCards[item]);
+        const thirdItemList = ['media', 'volunteers', 'vip'].map(item => badgeCards[item]);
 
         return (
             <div className="kanban-page">
@@ -41,6 +64,9 @@ class HackathonKanban extends Language {
                         </section>
                         <section className="badge-card-list-second">
                             <BadgeCardList itemList={secondItemList} />
+                        </section>
+                        <section>
+                            <HackathonProgress title="当前比赛进程" timeStart="2020/02/28" timeEnd="2020/03/08" mileStones={mockMilestones} />
                         </section>
                         <section className="badge-card-list-third">
                             <BadgeCardList itemList={thirdItemList} />
