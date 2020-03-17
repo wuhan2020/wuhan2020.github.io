58b841ecadc11a2fe05d4e220085baf5d562b380
Xin Wang <xin.victorwang@gmail.com>
2020-03-17 12:37:09 +0800

    remove gitalk #458 (#461)

--- src/pages/documentation/index.jsx
+++ src/pages/documentation/index.jsx
@@ -9,9 +9,6 @@ import Bar from '../../components/bar';
 import Sidemenu from '../../components/sidemenu';
 import Footer from '../../components/footer';
 import './index.scss';
-import GitalkComponent from "gitalk/dist/gitalk-component";
-import 'gitalk/dist/gitalk.css';
-
 
 // 锚点正则
 const anchorReg = /^#[^/]/;
@@ -93,7 +90,7 @@ class Documentation extends Language {
 
   render() {
     const language = this.getLanguage();
-    const { currentKey } = this.state; 
+    const { currentKey } = this.state;
     // 开发者页借助文档页载体
     const currentPage = currentKey === 'docs' ? 'docs' : 'develop';
     const dataSource = this.getLanguageDict(language, currentPage);
@@ -117,15 +114,6 @@ class Documentation extends Language {
             dangerouslySetInnerHTML={{ __html }}
           />
         </section>
-        <GitalkComponent options={{
-          clientID: "728da77f67226e477f03",
-          clientSecret: 'ec7e55e9a7a022900677599b776e4164c1fdd759',
-          repo: 'wuhan2020.github.io',
-          owner: 'wuhan2020',
-          admin: ['zhaofeng-shu33', 'iLtc', 'jeremy0519', 'lovepoem', 'chenrui333'],
-          id: 'wuhan2020-gitalk',
-          distractionFreeMode: false // Facebook-like distraction free mode
-        }} />
         <Footer logo="/images/wuhan2020-logo-gray.png" language={language} />
       </div>
     );
