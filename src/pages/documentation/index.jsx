f5399d4e584debe6e22de27a5743d538be59027a
Xin Wang <xin.victorwang@gmail.com>
2020-03-22 22:42:02 +0800

    add gitalk in docs module (#471)

--- src/pages/documentation/index.jsx
+++ src/pages/documentation/index.jsx
@@ -8,6 +8,8 @@ import Header from '../../components/header';
 import Bar from '../../components/bar';
 import Sidemenu from '../../components/sidemenu';
 import Footer from '../../components/footer';
+import GitalkComponent from "gitalk/dist/gitalk-component";
+import 'gitalk/dist/gitalk.css'
 import './index.scss';
 
 // 锚点正则
@@ -114,6 +116,15 @@ class Documentation extends Language {
             dangerouslySetInnerHTML={{ __html }}
           />
         </section>
+        <GitalkComponent options={{
+          clientID: "728da77f67226e477f03",
+          clientSecret: 'ec7e55e9a7a022900677599b776e4164c1fdd759',
+          repo: 'wuhan2020.github.io',
+          owner: 'wuhan2020',
+          admin: ['zhaofeng-shu33', 'iLtc', 'jeremy0519', 'lovepoem', 'chenrui333'],
+          id: 'wuhan2020-gitalk',
+          distractionFreeMode: false // Facebook-like distraction free mode
+        }} />
         <Footer logo="/images/wuhan2020-logo-gray.png" language={language} />
       </div>
     );
