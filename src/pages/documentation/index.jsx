978a0dd63405a329e81dabb96c3f90efd38ec69b
Xin Wang <xin.victorwang@gmail.com>
2020-03-23 00:42:13 +0800

    Revert "fix_id_bug (#482)" (#485)
    This reverts commit 6efe3713125cd2767bb6a625300a36373ddb7a6d.

--- src/pages/documentation/index.jsx
+++ src/pages/documentation/index.jsx
@@ -10,7 +10,6 @@ import Sidemenu from '../../components/sidemenu';
 import Footer from '../../components/footer';
 import GitalkComponent from "gitalk/dist/gitalk-component";
 import 'gitalk/dist/gitalk.css'
-import md5 from 'md5js'
 import './index.scss';
 
 // 锚点正则
@@ -117,17 +116,15 @@ class Documentation extends Language {
             dangerouslySetInnerHTML={{ __html }}
           />
         </section>
-        <section className="content-section">
-          <GitalkComponent options={{
-            clientID: "728da77f67226e477f03",
-            clientSecret: 'ec7e55e9a7a022900677599b776e4164c1fdd759',
-            repo: 'wuhan2020.github.io',
-            owner: 'wuhan2020',
-            admin: ['zhaofeng-shu33', 'iLtc', 'jeremy0519', 'lovepoem', 'chenrui333'],
-            id: md5(location.href),
-            distractionFreeMode: false // Facebook-like distraction free mode
-          }}/>
-        </section>
+        <GitalkComponent options={{
+          clientID: "728da77f67226e477f03",
+          clientSecret: 'ec7e55e9a7a022900677599b776e4164c1fdd759',
+          repo: 'website-comment',
+          owner: 'wuhan2020',
+          admin: ['zhaofeng-shu33', 'iLtc', 'jeremy0519', 'lovepoem', 'chenrui333'],
+          id: 'wuhan2020-gitalk',
+          distractionFreeMode: false // Facebook-like distraction free mode
+        }} />
         <Footer logo="/images/wuhan2020-logo-gray.png" language={language} />
       </div>
     );
