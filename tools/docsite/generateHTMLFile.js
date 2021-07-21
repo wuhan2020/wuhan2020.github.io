58d0f97a61a9b4a7f1416f722a682684487a34be
Arthur Ming <guobin.ming@pwc.com>
2021-07-21 16:27:57 +0800

    fix: 替换部分docsite内部实现

---
+++ tools/docsite/generateHTMLFile.js
@@ -0,0 +1,164 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs-extra');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const chalk = require('chalk');
+const ejs = require('ejs');
+const yaml = require('js-yaml');
+
+let siteConfig;
+try {
+  // 初始化时该文件还不存在
+  siteConfig = require(path.join(process.cwd(), './site_config/site')).default;
+} catch (err) {
+  // do nothing
+}
+let docsiteConfig;
+
+try {
+  const configPath = path.join(process.cwd(), 'docsite.config.yml');
+  if (fs.existsSync(configPath)) {
+    docsiteConfig = yaml.safeLoad(fs.readFileSync(configPath, 'utf8'));
+  }
+} catch (err) {
+  console.log(err);
+}
+
+const getConfigValue = (config, type, page, language, key) => {
+  try {
+    return config[type][page][language][key];
+  } catch (err) {
+    return '';
+  }
+};
+const generateHTMLFile = (env, cwd) => {
+  if (env === 'dev') {
+    window.rootPath = '';
+  } else if (env === 'prod') {
+    window.rootPath = siteConfig.rootPath;
+  }
+  // 生成404.html、重定向页面（用于初始进入的语言跳转）
+  if (fs.existsSync(path.join(cwd, './redirect.ejs'))) {
+    ejs.renderFile(
+      path.join(cwd, './redirect.ejs'),
+      {
+        defaultLanguage: siteConfig.defaultLanguage,
+        rootPath: window.rootPath,
+      },
+      (err, str) => {
+        if (err) {
+          console.log(chalk.red(err));
+          process.exit(1);
+        }
+        fs.writeFileSync(path.join(cwd, '404.html'), str, 'utf8');
+        fs.writeFileSync(path.join(cwd, 'index.html'), str, 'utf8');
+      }
+    );
+  }
+  // 生成首页（home），直接放置在语言目录下
+  if (fs.existsSync(path.join(cwd, './src/pages', 'home', './index.jsx'))) {
+    // 导入用ES6 export default导出的模块
+    const Page = require(path.join(cwd, './src/pages', 'home')).default;
+    // 生成英文版
+    fs.ensureDirSync(path.join(cwd, 'en-us'));
+    ejs.renderFile(
+      path.join(cwd, './template.ejs'),
+      {
+        env: env || 'prod',
+        title: getConfigValue(docsiteConfig, 'pages', 'home', 'en-us', 'title') || 'home',
+        keywords: getConfigValue(docsiteConfig, 'pages', 'home', 'en-us', 'keywords') || 'home',
+        description: getConfigValue(docsiteConfig, 'pages', 'home', 'en-us', 'description') || 'home',
+        rootPath: window.rootPath,
+        page: 'home',
+        __html: ReactDOMServer.renderToString(React.createElement(Page, { lang: 'en-us' }, null)),
+      },
+      (err, str) => {
+        if (err) {
+          console.log(chalk.red(err));
+          process.exit(1);
+        }
+        fs.writeFileSync(path.join(cwd, 'en-us', 'index.html'), str, 'utf8');
+      }
+    );
+
+    // 生成中文版
+    fs.ensureDirSync(path.join(cwd, 'zh-cn'));
+    ejs.renderFile(
+      path.join(cwd, './template.ejs'),
+      {
+        env: env || 'prod',
+        title: getConfigValue(docsiteConfig, 'pages', 'home', 'zh-cn', 'title') || 'home',
+        keywords: getConfigValue(docsiteConfig, 'pages', 'home', 'zh-cn', 'keywords') || 'home',
+        description: getConfigValue(docsiteConfig, 'pages', 'home', 'zh-cn', 'description') || 'home',
+        rootPath: window.rootPath,
+        page: 'home',
+        __html: ReactDOMServer.renderToString(React.createElement(Page, { lang: 'zh-cn' }, null)),
+      },
+      (err, str) => {
+        if (err) {
+          console.log(chalk.red(err));
+          process.exit(1);
+        }
+        fs.writeFileSync(path.join(cwd, 'zh-cn', 'index.html'), str, 'utf8');
+      }
+    );
+  }
+  // 生成除博客详情页（blogDetail）、文档页（documentation）首页（home）的其它页面
+  const pages = fs.readdirSync(path.join(cwd, './src/pages'));
+  pages.forEach(page => {
+    if (page === 'blogDetail' || page === 'documentation' || page === 'home') {
+      return;
+    }
+    // 是文件夹并且下面有index.jsx文件
+    if (fs.existsSync(path.join(cwd, './src/pages', page, './index.jsx'))) {
+      // 导入用ES6 export default导出的模块
+      const Page = require(path.join(cwd, './src/pages', page)).default;
+      // 生成英文版
+      fs.ensureDirSync(path.join(cwd, 'en-us', page));
+      ejs.renderFile(
+        path.join(cwd, './template.ejs'),
+        {
+          env: env || 'prod',
+          title: getConfigValue(docsiteConfig, 'pages', page, 'en-us', 'title') || page,
+          keywords: getConfigValue(docsiteConfig, 'pages', page, 'en-us', 'keywords') || page,
+          description: getConfigValue(docsiteConfig, 'pages', page, 'en-us', 'description') || page,
+          rootPath: window.rootPath,
+          page,
+          __html: ReactDOMServer.renderToString(React.createElement(Page, { lang: 'en-us' }, null)),
+        },
+        (err, str) => {
+          if (err) {
+            console.log(chalk.red(err));
+            process.exit(1);
+          }
+          fs.writeFileSync(path.join(cwd, 'en-us', page, 'index.html'), str, 'utf8');
+        }
+      );
+
+      // 生成中文版
+      fs.ensureDirSync(path.join(cwd, 'zh-cn', page));
+      ejs.renderFile(
+        path.join(cwd, './template.ejs'),
+        {
+          env: env || 'prod',
+          title: getConfigValue(docsiteConfig, 'pages', page, 'zh-cn', 'title') || page,
+          keywords: getConfigValue(docsiteConfig, 'pages', page, 'zh-cn', 'keywords') || page,
+          description: getConfigValue(docsiteConfig, 'pages', page, 'zh-cn', 'description') || page,
+          rootPath: window.rootPath,
+          page,
+          __html: ReactDOMServer.renderToString(React.createElement(Page, { lang: 'zh-cn' }, null)),
+        },
+        (err, str) => {
+          if (err) {
+            console.log(chalk.red(err));
+            process.exit(1);
+          }
+          fs.writeFileSync(path.join(cwd, 'zh-cn', page, 'index.html'), str, 'utf8');
+        }
+      );
+    }
+  });
+};
+module.exports = generateHTMLFile;
---
+++ tools/docsite/generateJSONFile.js
@@ -0,0 +1,157 @@
+'use strict';
+
+const path = require('path');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const chalk = require('chalk');
+const ejs = require('ejs');
+const fs = require('fs-extra');
+const parseMd = require('./parseMd.js');
+
+let siteConfig;
+try {
+  // 初始化时该文件还不存在
+  siteConfig = require(path.join(process.cwd(), './site_config/site')).default;
+} catch (err) {
+  // do noting
+}
+
+const generate = (env, cwd, type, lang, mdData) => {
+  if (env === 'dev') {
+    window.rootPath = '';
+  } else if (env === 'prod') {
+    window.rootPath = siteConfig.rootPath;
+  }
+  const parse = dir => {
+    const files = fs.readdirSync(dir);
+    files.forEach(file => {
+      const filepath = path.join(dir, file);
+      const stat = fs.statSync(filepath);
+      const extension = path.extname(file);
+      if (stat.isFile() && (extension === '.md' || extension === '.markdown')) {
+        const result = parseMd(filepath);
+        // cwd/docs/zh-cn/hello.md => cwd/zh-cn/docs/hello.json
+        const fileInfo = path.parse(filepath);
+        const splitArr = fileInfo.dir.split(`${type}${path.sep}${lang}`);
+        const targetPath = `${splitArr[0]}${lang}${path.sep}${type}${splitArr[1]}`;
+        fs.ensureDirSync(targetPath);
+        fs.writeFileSync(
+          path.join(targetPath, `${fileInfo.name}.json`),
+          JSON.stringify(
+            {
+              filename: file,
+              __html: result.__html,
+              link: path.join(targetPath.replace(`${cwd}`, ''), `${fileInfo.name}.html`), // 对应实际的html链接，未加前缀
+              meta: result.meta,
+            },
+            null,
+            2
+          ),
+          'utf8'
+        );
+        mdData[lang].push(
+          {
+            filename: file,
+            link: path.join(targetPath.replace(`${cwd}`, ''), `${fileInfo.name}.html`), // 对应实际的html链接，未加前缀
+            meta: result.meta,
+          }
+        );
+        // 同步生成HTML，避免再遍历一遍目录
+        // 生成文档
+        if (
+          type === 'docs' &&
+          fs.existsSync(path.join(cwd, './src/pages/documentation'))
+        ) {
+          const Documentation = require(path.join(cwd, './src/pages/documentation'))
+            .default;
+          ejs.renderFile(
+            path.join(cwd, './template.ejs'),
+            {
+              env: env || 'prod',
+              title: result.meta.title || fileInfo.name,
+              keywords: result.meta.keywords || fileInfo.name,
+              description: result.meta.description || fileInfo.name,
+              rootPath: window.rootPath,
+              page: 'documentation',
+              __html: ReactDOMServer.renderToString(
+                React.createElement(Documentation, { lang, __html: result.__html, meta: result.meta }, null)
+              ),
+            },
+            (err, str) => {
+              if (err) {
+                console.log(chalk.red(err));
+                process.exit(1);
+              }
+              fs.writeFileSync(path.join(targetPath, `${fileInfo.name}.html`), str, 'utf8');
+            }
+          );
+        } else if (
+          type === 'blog' &&
+          fs.existsSync(path.join(cwd, './src/pages/blogDetail'))
+        ) {
+          const BlogDetail = require(path.join(cwd, './src/pages/blogDetail')).default;
+          ejs.renderFile(
+            path.join(cwd, './template.ejs'),
+            {
+              env: env || 'prod',
+              title: result.meta.title || fileInfo.name,
+              keywords: result.meta.keywords || fileInfo.name,
+              description: result.meta.description || fileInfo.name,
+              rootPath: window.rootPath,
+              page: 'blogDetail',
+              __html: ReactDOMServer.renderToString(
+                React.createElement(BlogDetail, { lang, __html: result.__html, meta: result.meta }, null)
+              ),
+            },
+            (err, str) => {
+              if (err) {
+                console.log(chalk.red(err));
+                process.exit(1);
+              }
+              fs.writeFileSync(path.join(targetPath, `${fileInfo.name}.html`), str, 'utf8');
+            }
+          );
+        }
+      } else if (stat.isDirectory()) {
+        parse(filepath);
+      }
+    });
+  };
+  parse(path.join(cwd, type, lang));
+};
+const generateJSONFile = (env, cwd) => {
+  const blogData = {
+    'en-us': [],
+    'zh-cn': [],
+  };
+  const docsData = {
+    'en-us': [],
+    'zh-cn': [],
+  };
+  generate(env, cwd, 'docs', 'zh-cn', docsData);
+  generate(env, cwd, 'docs', 'en-us', docsData);
+  generate(env, cwd, 'blog', 'zh-cn', blogData);
+  generate(env, cwd, 'blog', 'en-us', blogData);
+  const mdDataPath = path.join(cwd, 'md_json');
+  fs.ensureDirSync(mdDataPath);
+  fs.writeFileSync(
+    path.join(mdDataPath, 'blog.json'),
+    JSON.stringify(
+      blogData,
+      null,
+      2,
+      'utf8'
+    )
+  );
+  fs.writeFileSync(
+    path.join(mdDataPath, 'docs.json'),
+    JSON.stringify(
+      docsData,
+      null,
+      2,
+      'utf8'
+    )
+  );
+};
+
+module.exports = generateJSONFile;
